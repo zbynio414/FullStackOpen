1d606246fae7361c957530807db20236de3502ef
Zbigniew Kowalkowski <zkowalkowski@gmail.com>
2023-01-26 15:20:25 +0100

    Part 0, Part 1, Part 2 till 2.10

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+
+const Display = props => <div>{props.value} </div>
+
+const Button = props => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+const Statistics = ({good,neutral,bad}) => {
+  const all=good+neutral+bad
+
+  return (
+  <table>
+    <tbody>
+      <tr><td>good</td><td>{good}</td></tr>
+      <tr><td>neutral</td><td>{neutral}</td></tr>
+      <tr><td>bad</td><td>{bad}</td></tr>
+      <tr><td>average</td><td>{((good-bad)/all).toFixed(1)}</td></tr>
+      <tr><td>positive</td><td>{(good/all*100).toFixed(1)}%</td></tr>
+    </tbody>
+  </table>
+)
+}
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  } 
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  } 
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  } 
+
+  return (
+    <div>
+      <br/>
+      <Display value='give feedback' />
+      <br/>
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral'/>
+      <Button handleClick={handleBadClick} text='bad'/>
+      <br/> 
+      <Display value='Statistics' />
+      <br/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        <ul>
+          {persons.map(person => <li key={person.name}>{person.name}</li>)}
+        </ul>
+      </ul>
+      <div>debug: {newName}</div>
+    </div>
+  )
+}
+
+export default App
+
