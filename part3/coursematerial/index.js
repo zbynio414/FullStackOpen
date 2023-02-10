7ea6ca7f21f22b03fd4dccf0a2a066d16e47facf
Zbigniew Kowalkowski <zkowalkowski@gmail.com>
2023-02-10 16:44:48 +0100

    Coursematerial error handling

--- part3/coursematerial/index.js
+++ part3/coursematerial/index.js
@@ -17,7 +17,15 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes/:id',(request, response) => {
     Note.findById(request.params.id).then(note => {
+      if (note) {
         response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({error: 'malformatted id'})
     })
 })
 
