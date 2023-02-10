cd694abcccb6d0545a76eabda05095ddf77e4f50
Zbigniew Kowalkowski <zkowalkowski@gmail.com>
2023-02-10 12:10:37 +0100

    Coursematerial Part 3-4 MongoDB work!

--- part3/coursematerial/index.js
+++ part3/coursematerial/index.js
@@ -1,28 +1,14 @@
+require('dotenv').config()
 const express = require('express')
-const nodemon = require('nodemon')
 const app= express()
+const Note = require('./models/note')
 
 app.use(express.json())
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
   })
 
 app.get('/', (request, response) => {
@@ -30,18 +16,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes/:id',(request, response) => {
-    const id = Number(request.params.id)
-    console.log(id);
-    const note = notes.find(note => {
-        console.log(note.id, typeof note.id, id, typeof id, note.id ===id)
-        return note.id === id
-    })
-    console.log(note);
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -50,12 +27,7 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-    return maxId +1
-}
+
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -65,19 +37,17 @@ app.post('/api/notes', (request, response) => {
       error: 'content missing'
     })
   }
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId
-  }
-  console.log(note);
-  notes = notes.concat(note)
-  response.json(note)
+  })
+  //console.log(note);
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ part3/coursematerial/models/note.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to ', url);
+
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB');
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message0)
+    })
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+    transform: (document, returnObject) => {
+        returnObject.id = returnObject._id.toString()
+        delete returnObject._id
+        delete returnObject.__v
+    }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
