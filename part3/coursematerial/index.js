2f83a083ab18503f852ca20f6eac6b6a4571c549
Zbigniew Kowalkowski <zkowalkowski@gmail.com>
2023-02-13 15:29:07 +0100

    Course material data validation with Mongoose

--- part3/coursematerial/index.js
+++ part3/coursematerial/index.js
@@ -18,7 +18,10 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
   }
+
   next(error)
 }
 
@@ -76,20 +79,21 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
   })
   //console.log(note);
-  note.save().then(savedNote => {
-    response.json(savedNote)
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
   })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
-  
-  const note = {
-  content: body.content,
-  important: body.important
-  }
+  const { content, important} = request.body
 
-  Note.findByIdAndUpdate(request.params.id, note, {new: true})
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    {content, important}, 
+    {new: true, runValidators: true, context: 'query'}
+    )
     .then(updatedNote => {
       response.json(updatedNote)
     })
