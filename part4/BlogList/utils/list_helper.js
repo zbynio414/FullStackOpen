f4f9c9d1a222cc8b3dd233f2c73112563efb999b
Zbigniew Kowalkowski <zkowalkowski@gmail.com>
2023-02-15 16:46:10 +0100

    4.5*: helper functions and unit tests, step3
    Define a new favoriteBlog function that receives a list
    of blogs as a parameter.

--- part4/BlogList/utils/list_helper.js
+++ part4/BlogList/utils/list_helper.js
@@ -10,11 +10,15 @@ const totalLikes = (listOfBlogs) => {
   return listOfBlogs.reduce(reducer, 0)
 }  
 
-
-
+const favoriteBlog = (listOfBlogs) => {
+  const likesOnly = listOfBlogs.map( blog => blog.likes)
+  const { title, author, likes } = listOfBlogs[likesOnly.indexOf(Math.max(...likesOnly))]
+  return { title, author, likes }
+}
 
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
 
