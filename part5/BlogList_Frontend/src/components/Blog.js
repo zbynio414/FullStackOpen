a57061d554f71e21b1f6687240590a5f5fc9866e
Zbigniew Kowalkowski <zkowalkowski@gmail.com>
2023-02-25 14:45:05 +0100

    Ex.5.7-Add View button to each Blog

--- part5/BlogList_Frontend/src/components/Blog.js
+++ part5/BlogList_Frontend/src/components/Blog.js
@@ -1,7 +1,35 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const blogStyle = {
+    paddingTop:10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const showWhenVisible = { display: visible ? '' : 'none'}
+  
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  } 
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      <button onClick={toggleVisibility}>{visible ? 'hide' : 'view' }</button>
+      <div style={showWhenVisible}>
+        <div>{blog.url}</div>
+        <div>Likes: {blog.likes} 
+          <button>like</button>
+        </div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
