8953c4fe479a419b2481c701a69cf3835223ca08
Zbigniew Kowalkowski <zkowalkowski@gmail.com>
2023-02-27 10:04:31 +0100

    Add Authorization to logger

--- part4/BlogList/utils/middleware.js
+++ part4/BlogList/utils/middleware.js
@@ -3,6 +3,7 @@ const logger = require('./logger')
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
   logger.info('Path:  ', request.path)
+  logger.info('Authorization:  ', request.get('authorization'))
   logger.info('Body:  ', request.body)
   logger.info('---')
   next()
