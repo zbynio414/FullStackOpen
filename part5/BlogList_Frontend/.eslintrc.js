c016df410fe16101eb27e299924d4c2ee436dc33
Zbigniew Kowalkowski <zkowalkowski@gmail.com>
2023-03-03 10:12:34 +0100

    add eslint-plugin-testing-library

--- part5/BlogList_Frontend/.eslintrc.js
+++ part5/BlogList_Frontend/.eslintrc.js
@@ -19,6 +19,12 @@ module.exports = {
   'plugins': [
     'react', 'jest'
   ],
+  'overrides': [
+    {
+      'files': ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      'extends': ['plugin:testing-library/react'],
+    }
+  ],
   'rules': {
     'indent': [
       'error',
@@ -46,7 +52,11 @@ module.exports = {
     ],
     'no-console': 0,
     'react/prop-types': 0,
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'testing-library/await-async-query': 'error',
+    'testing-library/no-await-sync-query': 'error',
+    'testing-library/no-debugging-utils': 'warn',
+    'testing-library/no-dom-import': 'off'
   },
   'settings': {
     'react': {
--- part5/BlogList_Frontend/src/components/Blog.test.js
+++ part5/BlogList_Frontend/src/components/Blog.test.js
@@ -1,21 +1,24 @@
+/* eslint-disable testing-library/no-container */
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
+const blog = {
+  title: 'TestTitle',
+  author: 'Tester',
+  url: 'www.co.pl',
+  likes: 6,
+  user: {
+    username: 'root',
+    name: 'Superuser',
+    id: '63f4d191b814057ef556d358'
+  },
+  id: '63f76497852d7462fce98a75'
+}
+
 test('render blog content', () => {
-  const blog = {
-    title: 'TestTitle',
-    author: 'Tester',
-    url: 'www.co.pl',
-    likes: 6,
-    user: {
-      username: 'root',
-      name: 'Superuser',
-      id: '63f4d191b814057ef556d358'
-    },
-    id: '63f76497852d7462fce98a75'
-  }
 
   const { container } = render(<Blog blog={blog} isMine={true} />)
 
@@ -29,4 +32,21 @@ test('render blog content', () => {
 
   expect(url_likes).toHaveStyle('display: none')
   expect(url_likes).toHaveStyle('display: none')
+})
+
+test('clicking the button show the url and author', async () => {
+
+  const mockHandler = jest.fn()
+
+  const user = userEvent.setup()
+
+  render(
+    <Blog blog={blog} isMine={true} togggleVisibility={mockHandler} />
+  )
+
+  const button = screen.getByText('view')
+
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
