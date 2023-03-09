f40f7742632694f5492808e7c9aa2d15fad7ed2d
Zbigniew Kowalkowski <zkowalkowski@gmail.com>
2023-03-09 16:41:55 +0100

    sort anecdotes by votes

--- part6/redux-anecdotes/src/components/Anecdotes.js
+++ part6/redux-anecdotes/src/components/Anecdotes.js
@@ -19,11 +19,15 @@ const Anecdotes = () => {
     const dispatch = useDispatch()
     const anecdotes = useSelector(state => state)
 
+    const sortedAnecdotes = [...anecdotes]
+    .sort((a, b) => b.votes - a.votes)
+
+
     return (
         <div>
           <h2>Anecdotes</h2>
           <ul>
-            {anecdotes.map(anecdote =>
+            {sortedAnecdotes.map(anecdote =>
             <Anecdote
                 key={anecdote.id}
                 anecdote={anecdote}
