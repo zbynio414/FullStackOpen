312215faa9d24b51fd2e06358c542402fcaee81d
Zbigniew Kowalkowski <zkowalkowski@gmail.com>
2023-03-10 09:03:04 +0100

    6.9 added filtering anecdotes

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,12 +1,14 @@
 
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import Filter from './components/Filter'
 
 const App = () => {
 
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Filter />
       <AnecdoteList /> 
       <AnecdoteForm />
     </div>
--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -17,7 +17,9 @@ const Anecdote = ({ anecdote, handleVote }) => {
 
 const Anecdotes = () => {
     const dispatch = useDispatch()
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(({ anecdotes, filter }) => {
+      return anecdotes.filter(a => a.content.toLowerCase().includes(filter.toLowerCase()))
+    })
 
     const sortedAnecdotes = [...anecdotes]
     .sort((a, b) => b.votes - a.votes)
