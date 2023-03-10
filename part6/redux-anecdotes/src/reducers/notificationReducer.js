c20cb9b17e984c53e1c432449cf04de302522e7c
Zbigniew Kowalkowski <zkowalkowski@gmail.com>
2023-03-10 12:42:28 +0100

    noification reducer created

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const notificationSlice = createSlice({
+    name: 'notification',
+    initialState: '',
+    reducers: {
+        createNotification(state, action) {
+            return action.payload
+        },
+        deleteNotification(state, action) {
+            return ''
+        }
+    }
+})
+
+export const { createNotification, deleteNotification } = notificationSlice.actions
+export default notificationSlice.reducer
