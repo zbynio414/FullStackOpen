b4564d16bfaa9b60ce1154bf402f97e4cc0be397
Zbigniew Kowalkowski <zkowalkowski@gmail.com>
2023-03-10 13:45:38 +0100

    Notification component

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,19 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
 
-// import anecdoteReducer from './reducers/anecdoteReducer'
-// import filterReducer from './reducers/filterReducer'
 import store from "./store";
-
-// const reducer = combineReducers({
-//   anecdotes: anecdoteReducer,
-//   filter: filterReducer
-// })
-
-// const store = createStore(reducer)
 
 console.log(store.getState())
 
