1d7a5ac069d2a8906697ac8660f3ff6af7c24265
Zbigniew Kowalkowski <zkowalkowski@gmail.com>
2023-03-13 12:37:46 +0100

    6.14  fetch the anecdotes from the backend

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit"
 
 const notificationSlice = createSlice({
     name: 'notification',
-    initialState: 'initial value',
+    initialState: '',
     reducers: {
         createNotification(state, action) {
             return action.payload
