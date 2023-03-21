ed3f7093efa3dcf3657b151f99933e465a4d3630
Zbigniew Kowalkowski <zkowalkowski@gmail.com>
2023-03-21 10:25:19 +0100

    first query

--- part13/postgres-notes/index.js
+++ part13/postgres-notes/index.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, QueryTypes } = require('sequelize')
 
 const sequelize =  new Sequelize(process.env.DATABASE_URL)
 
 const main = async () => {
     try {
         await sequelize.authenticate()
-        console.log('Connection has been established successfully');
+        const notes = await sequelize
+            .query("SELECT * FROM notes", { type: QueryTypes.SELECT })
+        console.log(notes)
         sequelize.close()
     } catch (error) {
         console.log('Unable to connect to the database:', error);
